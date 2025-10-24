79744239f55586d638ed26db089920d4dbe76b15
AIssah459 <96153984+AIssah459@users.noreply.github.com>
2025-10-24 19:00:38 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -10,14 +10,31 @@ dotenv.config({path: '../../.env'});
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// app.use(cors({
-//   origin: ['http://localhost:5173','http://localhost:8080'],
-//   //origin: true,
-//   credentials: true,
-//   methods: ['GET','POST','OPTIONS','DELETE'],
-//   allowedHeaders: ['Content-Type','Authorization']
-// }));
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:8080',
+  'https://projectx-app.up.railway.app',
+  'https://project-x-20h.pages.dev'
+];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log('CORS request origin:', origin);
+    // allow requests with no origin like mobile apps or curl
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  //origin: true,
+  credentials: true,
+  methods: ['GET','POST','OPTIONS','DELETE'],
+  allowedHeaders: ['Content-Type','Authorization']
+};
+
+app.use(cors(corsOptions));
 const isProduction = process.env.NODE_ENV === 'production';
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,35 +4,6 @@ import cors from 'cors';
 
 const router = Router();
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:8080',
-  'https://projectx-app.up.railway.app',
-  'https://project-x-20h.pages.dev'
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log('CORS request origin:', origin);
-    // allow requests with no origin like mobile apps or curl
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  //origin: true,
-  credentials: true,
-  methods: ['GET','POST','OPTIONS','DELETE'],
-  allowedHeaders: ['Content-Type','Authorization']
-};
-
-router.use(cors(corsOptions));
-router.options('*', cors(corsOptions));
-
-
-
 router.post('/login', authController.loginPost);
 router.post('/logout', authController.logoutPost);
 router.post('/signup', authController.signupPost);
